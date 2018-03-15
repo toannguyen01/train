8a8392c1bbeba900ad22e5fa842409c5da5c3333
nguyen <ductoannguyen02@gmail.com>
2018-03-15 16:17:58 +0700

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315085659) do
+ActiveRecord::Schema.define(version: 20180315091042) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180315085659) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.boolean "published"
     t.integer "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
