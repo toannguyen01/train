90ab2c0c18e60657fd150b034d633bc6d0ab4b68
nguyen <ductoannguyen02@gmail.com>
2018-03-15 15:59:11 +0700

    add referent mot nhieu

---
+++ db/migrate/20180315085659_ad_reference_to_categories_in_products.rb
@@ -0,0 +1,5 @@
+class AdReferenceToCategoriesInProducts < ActiveRecord::Migration[5.1]
+  def change
+  	add_reference :products, :category, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315083603) do
+ActiveRecord::Schema.define(version: 20180315085659) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
